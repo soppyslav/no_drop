d9f9c91eb303cbafa71d024f57e879969d057339
Colin Slavtcheff <50255821+soppyslav@users.noreply.github.com>
2019-09-22 21:48:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Crypto.Cipher import AES
 with open('engwordlist.txt') as f:
    for line in f:
+      key = line
+      iv = 'aabbccddeeff00998877665544332211'
       
-       if 'str' in line:
-          break
