03626ea7b2c9c4a25b09299df500fb92a1916812
Colin Slavtcheff <50255821+soppyslav@users.noreply.github.com>
2019-09-23 18:46:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from Crypto.Cipher import AES
 with open('engwordlist.txt') as f:
    for line in f:
+      line = line.ljust(16,'#')
       key = line
       iv = 'aabbccddeeff00998877665544332211'
-      
+      ciphertext = '764aa26b55a4da654df6b19e4bce00f4ed05e09346fb0e762583cb7da2ac93a2'
+      aes = AES.new(key, AES.MODE_CBC, iv)
+      encd = aes.encrypt(line)
+      print(encd)
